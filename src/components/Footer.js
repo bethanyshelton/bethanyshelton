835916620df9ac4e4a0897be00f94144829a7170
Bethany Shelton <59832223+bethanyshelton@users.noreply.github.com>
2021-06-20 12:49:22 -0400

    Simplify design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import React from "react";
 function Footer() {
 	return (
 		<div>
-			<p>Video by Kelly Lacy from Pexels</p>
-			<p>Photo by Tim Mossholder from Pexels</p>
+			<p>%copy current year Bethany Shelton</p>
 		</div>
 	);
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,14 +3,19 @@ import React from "react";
 import "../App.css";
 import "./Hero.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
 function Hero() {
 	return (
 		<div className="hero-container">
-			<video src="video.mp4" autoPlay="true" loop="true" muted="true"></video>
 			<div className="title-container">
-				<h1 className="title">Hi, I'm Bethany</h1>
+				<h1 className="title">Hello there, I'm Bethany</h1>
 				<h2 className="subtitle">a Web Developer</h2>
-				<p className="scroll">Scroll down to see what I can do</p>
+				<div className="scroll">
+					<p>Scroll down to see what I can do</p>
+					<FontAwesomeIcon className="chevron-down" icon={faChevronDown} />
+				</div>
 			</div>
 		</div>
 	);
