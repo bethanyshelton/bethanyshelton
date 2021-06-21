a4d064333a7b0a4700b798e27ce7e6c65e7e3064
Bethany Shelton <59832223+bethanyshelton@users.noreply.github.com>
2021-06-21 09:03:30 -0400

    Add and style contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,39 @@
 import React from "react";
 
 import "../App.css";
+import "./Contact.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 
 function Contact() {
 	return (
-		<div>
-			<h1>This is how to contact me</h1>
+		<div className="contact">
+			<h1>Send me a message</h1>
+			<form action="post" className="contact-form">
+				<input
+					className="input-box"
+					type="text"
+					placeholder="Your Name"
+					required
+				/>
+				<input
+					className="input-box"
+					type="email"
+					placeholder="Your Email"
+					required
+				/>
+				<input
+					className="input-box"
+					type="textarea"
+					placeholder="Message"
+					required
+				/>
+				<button className="input-box" type="submit">
+					Send
+					<FontAwesomeIcon className="icon" icon={faPaperPlane} />
+				</button>
+			</form>
 		</div>
 	);
 }
