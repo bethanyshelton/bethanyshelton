d81147e5fde8aa110673cad7861aad9f75275c9b
Bethany Shelton <59832223+bethanyshelton@users.noreply.github.com>
2021-06-21 10:10:27 -0400

    Complete footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,29 @@
 import React from "react";
 
+import "./Footer.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faGithubSquare,
+	faLinkedin,
+	faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons";
+
+const currentYear = new Date().getFullYear();
+
 function Footer() {
 	return (
-		<div>
-			<p>%copy current year Bethany Shelton</p>
+		<div className="footer">
+			<div className="footer-container">
+				<p className="copyright">
+					&copy; {currentYear} <span className="name"> Bethany Shelton</span>
+				</p>
+				<div className="social">
+					<FontAwesomeIcon className="icon" icon={faGithubSquare} />
+					<FontAwesomeIcon className="icon" icon={faLinkedin} />
+					<FontAwesomeIcon className="icon" icon={faTwitterSquare} />
+				</div>
+			</div>
 		</div>
 	);
 }
